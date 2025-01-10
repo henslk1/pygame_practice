3ce698816ac60398258400eaa3064fb2bc653335
Kristin <knhbjr@gmail.com>
2025-01-10 13:40:33 -0600

    Added color values

---
+++ Modules/colors.py
@@ -0,0 +1,17 @@
+#colors for matching game
+
+class Colors():
+    "Defining the color used in other modules."
+
+    def __init__(self):
+        """Color Attributes."""
+        self.gray = (100, 100, 100)
+        self.navy_blue = (60, 60, 100)
+        self.white = (255, 255, 255)
+        self.red = (255, 0, 0)
+        self.green = (0, 255, 0)
+        self.blue = (0, 0, 255)
+        self.yellow = (255, 255, 0)
+        self.orange = (255, 128, 0)
+        self.purple = (255, 0, 255)
+        self.cyan = (0, 255, 255)
