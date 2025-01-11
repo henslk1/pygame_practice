c6bba4be203b54e1408d61a5b85a845d8bd199c2
Kristin <knhbjr@gmail.com>
2025-01-11 15:44:03 -0600

    Defined shape strings.

---
+++ Modules/shapes.py
@@ -0,0 +1,14 @@
+#define shapes for matching game
+
+class Shapes():
+    """Define shapes."""
+
+    def __init__(self):
+        """Initiate shape attributes."""
+        self.donut = 'donut'
+        self.square = 'square'
+        self.diamond = 'diamond'
+        self.lines = 'lines'
+        self.oval = 'oval'
+
+        self.all_shapes = (self.donut, self.square, self.diamond, self.lines, self.oval)
