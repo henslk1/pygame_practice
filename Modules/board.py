3858a425aeb45928ad856bf64278a72d86aa112d
Kristin <knhbjr@gmail.com>
2025-01-11 17:36:43 -0600

    Added methods for generating a new, random board.

--- Modules/board.py
+++ Modules/board.py
@@ -1,5 +1,8 @@
 #board functions
 from settings import Settings
+from shapes import Shapes
+from colors import Colors
+import random
 
 class Board():
     """Represents the board."""
@@ -8,7 +11,11 @@ class Board():
         """Initiate attributes."""
 
         self.settings = Settings(self)
+        self.colors = Colors(self)
+        self.shapes = Shapes(self)
+
         self.define_board_margins()
+
 
     def define_board_margins(self):
         """Define x and y margin."""
@@ -17,3 +24,42 @@ class Board():
                              * (self.settings.box_size + self.settings.gap_size))) / 2)
         self.y_margin = int((self.settings.window_height - (self.settings.board_height
                              * (self.settings.box_size + self.settings.gap_size))) / 2)
+
+
+    def get_randomized_board(self):
+        
+        self._generate_icons()
+        self._create_board_data_structure(self.icons)
+
+
+    def _generate_icons(self):
+        """Get a list of every posible shape and color."""
+
+        self.icons = []
+
+        for color in self.colors.all_colors:
+            for shape in self.shapes.all_shapes:
+                self.icons.append( (shape, color) )
+
+        random.shuffle(self.icons) # randomize the order of the icons
+        self.num_icons = int(self.settings.board_width * self.settings.board_height / 2)
+        self.icons = self.icons[:self.num_icons] * 2 # makes 2 of each
+        random.shuffle(self.icons)
+
+        return self.icons
+
+
+    def _create_board_data_structure(self, icons):
+        """Create board structure with randomly placed icons."""
+
+        self.board = []
+        for x in range(self.settings.board_width):
+            self.column = []
+            for y in range(self.settings.board_height):
+                self.column.append(icons[0])
+                del icons[0] #remove icons as they're assigned
+            
+            self.board.append(self.column)
+        
+        return self.board
+    
