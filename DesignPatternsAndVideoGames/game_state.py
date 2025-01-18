91e5b6d4b29a6806074509443c5e5505d18e4525
Kristin <knhbjr@gmail.com>
2025-01-17 20:23:20 -0600

    Rewrote GameState to use the unit module

--- DesignPatternsAndVideoGames/game_state.py
+++ DesignPatternsAndVideoGames/game_state.py
@@ -1,4 +1,5 @@
 from pygame.math import Vector2
+from units import Tank, Tower
 
 
 class GameState():
@@ -8,27 +9,17 @@ class GameState():
         """Initialize attributes"""
 
         self.world_size = Vector2(16,10)
-        self.tank_pos = Vector2(5,4)
-        self.towers_pos = [
-            Vector2(10,3),
-            Vector2(10,5)
-            ]
+        self.units = [
+            Tank(self, Vector2(5,4), Vector2(1,0)),
+            Tower(self, Vector2(10,3), Vector2(0,1)),
+            Tower(self, Vector2(10,5), Vector2(0,1))
+
+        ]
 
 
     def update(self, move_tank_command):
         """Update coords"""
 
-        new_tank_pos = self.tank_pos + move_tank_command
-        
-        if new_tank_pos.x < 0 or new_tank_pos.x >= self.world_size.x \
-        or new_tank_pos.y < 0 or new_tank_pos.y >= self.world_size.y:
+        for unit in self.units:
             
-            return
-        
-        for position in self.towers_pos:
-
-            if new_tank_pos == position:
-
-                return
-            
-        self.tank_pos = new_tank_pos
+            unit.move(move_tank_command)
